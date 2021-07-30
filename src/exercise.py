71e540f844e1ed3f9ef4999e534b755d4e221e66
Justin Turner <jmturner638@gmail.com>
2021-07-30 08:33:56 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    n = int(input("Give a number:"))
+    product = 1
+
+    for i in range(1, n+1):
+        product *= i
+
+    print("Factorial: " + str(product))
 
 if __name__ == '__main__':
     main()
